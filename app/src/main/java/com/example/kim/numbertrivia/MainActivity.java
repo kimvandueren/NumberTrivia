04f8eea840c320d3a0ef61f54b20feb1ef9a4842
Kim van Dueren den Hollander <Kim.van.Dueren.den.Hollander@hva.nl>
2018-11-26 11:06:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kim/numbertrivia/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.kim.numbertrivia;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<TriviaItem> mTriviaItems = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new RecyclerViewAdapter(this, mTriviaItems);
+        mRecyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/kim/numbertrivia/RecyclerViewAdapter.java
@@ -0,0 +1,51 @@
+package com.example.kim.numbertrivia;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    public List<TriviaItem> listTriviaItem;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView numberText;
+        public TextView triviaText;
+        public View view;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            numberText = itemView.findViewById(R.id.numberText);
+            triviaText = itemView.findViewById(R.id.triviaText);
+            view = itemView;
+        }
+    }
+
+    public RecyclerViewAdapter(Context context, List<TriviaItem> listTriviaItem) {
+        this.context = context;
+        this.listTriviaItem = listTriviaItem;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_cell, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final TriviaItem triviaItem = listTriviaItem.get(position);
+        holder.numberText.setText("23");
+        holder.triviaText.setText("test");
+    }
+
+    @Override
+    public int getItemCount() {
+        return listTriviaItem.size();
+    }
+}
